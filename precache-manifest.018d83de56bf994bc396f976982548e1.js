7d11c3c76fbf4cf1917e1a8bd3214c5d1e375244
Eltonleao <3lton.leao@gmail.com>
2019-11-17 12:31:55 -0300

    first deploy

---
+++ precache-manifest.018d83de56bf994bc396f976982548e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729e13f7e3614a6eff3637552ac9fdb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66267c8d691141559b37",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "171634311ebc4dc5dd70",
+    "url": "/static/js/2.f1ebb5ef.chunk.js"
+  },
+  {
+    "revision": "66267c8d691141559b37",
+    "url": "/static/js/main.746668f1.chunk.js"
+  },
+  {
+    "revision": "a162fd26569a25a9e0f7",
+    "url": "/static/js/runtime-main.c032d416.js"
+  }
+]);
